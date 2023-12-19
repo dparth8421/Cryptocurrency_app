eecfcaad9d721e4bb05e87eae0bbd97fbab6f092
dparth8421 <dparth388@gmail.com>
2023-12-19 20:40:00 +0530

    Setup project && Added Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CoinPage from "./pages/CoinPage";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/coins/:id",
+      element: <CoinPage />,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header />
+        <RouterProvider router={appRouter}></RouterProvider>
+      </div>
     </div>
   );
 }
