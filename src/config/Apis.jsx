751b1ef64a72911de46cbeeeb71431c833bf1c4c
dparth8421 <dparth388@gmail.com>
2023-12-22 01:50:14 +0530

    Created Table compfor coins && added styles

--- src/config/Apis.jsx
+++ src/config/Apis.jsx
@@ -1,5 +1,8 @@
-export const CoinList = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+export const CoinList = (currency) => {
+  const vsCurrency = currency || "usd";
+
+  return `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vsCurrency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+};
 
 export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
